7d927335eafa709fe49cb697fc227ff7a73cde6f
Buranovv <zahiriddinburanov007@gmail.com>
2023-07-29 19:25:39 +0500

    d

--- main.js
+++ main.js
@@ -1,53 +1,5 @@
-/*Karra-kara*/
-// let num = Number(prompt("Son kiriting..."));
-
-// if (num > 0) {
-//   for (let i = 0; i < 11; i++) {
-//     console.log(`${num} * ${i} = ${num*i}`);
-//   }
-// }
-
-/* Tub son */
-// let n = Number(prompt("Son kiriting..."));
-// let isPrime;
-
-// for (let i = 2; i <= n; i++) {
-//   for (let j = 2; j < i; j++) {
-//     isPrime = 1;
-//     if (i % j === 0) {
-//       isPrime = 0;
-//       break;
-//     }
-//   }
-//   if (isPrime === 1) {
-//     console.log(i);
-//   }
-// }
-
-/*Kalkulator*/
-let num1 = Number(prompt("1-sonni kiriting..."));
-let num2 = Number(prompt("2-sonni kiriting..."));
-let symb = prompt("Amal kiriting...");
-console.log(typeof symb);
-switch (symb) {
-  case "+":
-    alert(num1 + num2);
-    break;
-  case "-":
-    alert(num1 - num2);
-    break;
-  case "*":
-    alert(num1 * num2);
-    break;
-  case "/":
-    alert(num1 / num2);
-    break;
-  case "%":
-    alert(num1 % num2);
-    break;
-  case "**":
-    alert(num1 ** num2);
-    break;
-  default:
-    break;
+for (let i = 100; i > 1; i--) {
+  if (i % 5 == 0) {
+    console.log(i);
+  }
 }
