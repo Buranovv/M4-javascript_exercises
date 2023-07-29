44f855ef641229c74031b478f2e44e5dec1daf8b
Buranovv <zahiriddinburanov007@gmail.com>
2023-07-29 20:53:58 +0500

    FIZZBUZZ

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-for (let i = 100; i > 1; i--) {
-  if (i % 5 == 0) {
-    console.log(i);
-  }
+// let num = Number(prompt("Son kiriting..."));
+
+// for (let i = num; i > 1; i--) {
+//   if (i % 5 == 0 || i % -5 == 0) {
+//     console.log(i);
+//   }
+// }
+
+//Homework
+
+//FIZZBUZZ
+let num = 22;
+
+if (num % 5 === 0 && num % 3 === 0) {
+  console.log("FIZZBUZZ");
+} else if (num % 3 === 0) {
+  console.log("FIZZ");
+} else if (num % 5 === 0) {
+  console.log("BUZZ");
+} else {
+  console.log(num);
 }
