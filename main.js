af4885c2abe6168c0d3044e4e304a533db9f6774
Buranovv <zahiriddinburanov007@gmail.com>
2023-08-01 00:10:13 +0500

    Kim birinchi borishga!

--- main.js
+++ main.js
@@ -9,14 +9,45 @@
 //Homework
 
 //FIZZBUZZ
-let num = 22;
+// let num = 22;
 
-if (num % 5 === 0 && num % 3 === 0) {
-  console.log("FIZZBUZZ");
-} else if (num % 3 === 0) {
-  console.log("FIZZ");
-} else if (num % 5 === 0) {
-  console.log("BUZZ");
-} else {
-  console.log(num);
-}
+// if (num % 5 === 0 && num % 3 === 0) {
+//   console.log("FIZZBUZZ");
+// } else if (num % 3 === 0) {
+//   console.log("FIZZ");
+// } else if (num % 5 === 0) {
+//   console.log("BUZZ");
+// } else {
+//   console.log(num);
+// }
+
+const distance = document.querySelector(".input");
+const onFood = document.querySelector(".ptext");
+const onBike = document.querySelector(".vtext");
+const onCar = document.querySelector(".mtext");
+const onPlane = document.querySelector(".stext");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  let onFoodHour = distance.value / 3.6;
+  let onFoodHourFix = Math.floor(distance.value / 3.6);
+  let foMin = Math.abs((onFoodHour - onFoodHourFix).toFixed(1) * 60);
+
+  let onBikeHour = distance.value / 20.1;
+  let onBikeHourFix = Math.floor(distance.value / 20.1);
+  let biMin = Math.abs((onBikeHour - onBikeHourFix).toFixed(1) * 60);
+
+  let onCarHour = distance.value / 70;
+  let onCarHourFix = Math.floor(distance.value / 70);
+  let caMin = Math.abs((onCarHour - onCarHourFix).toFixed(1) * 60);
+
+  let onPlaneHour = distance.value / 800;
+  let onPlaneHourFix = Math.floor(distance.value / 800);
+  let plMin = Math.abs((onPlaneHour - onPlaneHourFix).toFixed(1) * 60);
+
+  onFood.innerHTML = `${Math.floor(distance.value / 3.6)} soat ${foMin} minut`;
+  onBike.innerHTML = `${Math.floor(distance.value / 20.1)} soat ${biMin} minut`;
+  onCar.innerHTML = `${Math.floor(distance.value / 70)} soat ${caMin} minut`;
+  onPlane.innerHTML = `${Math.floor(distance.value / 800)} soat ${plMin} minut`;
+});
