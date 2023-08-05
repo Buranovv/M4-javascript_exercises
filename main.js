25b5621ddfb3a4cc78f2206e04e508fb89d4bb0a
Buranovv <zahiriddinburanov007@gmail.com>
2023-08-05 21:00:09 +0500

    some

--- main.js
+++ main.js
@@ -1,35 +1,7 @@
-const elName = document.querySelector(".name");
-const elRelationship = document.querySelector(".relationship");
-const elPhone = document.querySelector(".phone");
-const elForm = document.querySelector("#form");
-const elCards = document.querySelector(".cards");
+const elToDo = document.querySelector("#todo");
+const elDate = document.querySelector("#date");
+const elList = document.querySelector(".todoList");
 
-elForm.addEventListener("submit", function (evt) {
-  evt.preventDefault();
+const toDoList = [];
 
-  if (elName.value && elRelationship.value && elPhone.value !== "") {
-    const newCard = document.createElement("div");
-    newCard.className = "card";
-    newCard.innerHTML = `
-    <h4>${elName.value}</h4>
-    <p class="relation">${elRelationship.value}</p>
-    <p id="phone">${elPhone.value}</p>
-    `;
-    elCards.appendChild(newCard);
-    elForm.reset();
-  } else {
-    alert("Error");
-  }
-});
 
-// let num = [1, 3, 5, 9];
-
-// function numbersNum(array) {
-//   let sum = 0;
-
-//   for (let i = 0; i < array.length; i++) {
-//     sum += array[i];
-//   }
-//   return sum;
-// }
-// console.log(numbersNum(num));
