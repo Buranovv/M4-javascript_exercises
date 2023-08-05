456c136dfb151b7cb4d1b89860776d70b6ccf5b6
Buranovv <zahiriddinburanov007@gmail.com>
2023-08-05 19:28:49 +0500

    fixed

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
-// const elName = document.querySelector(".name");
-// const elRelationship = document.querySelector(".relationship");
-// const elPhone = document.querySelector(".phone");
-// const elForm = document.querySelector("#form");
-// const elCards = document.querySelector(".cards");
+const elName = document.querySelector(".name");
+const elRelationship = document.querySelector(".relationship");
+const elPhone = document.querySelector(".phone");
+const elForm = document.querySelector("#form");
+const elCards = document.querySelector(".cards");
 
-// elForm.addEventListener("submit", function (evt) {
-//   evt.preventDefault();
+elForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
 
-//   if (elName.value && elRelationship.value && elPhone.value !== "") {
-//     const newCard = document.createElement("div");
-//     newCard.className = "card";
-//     newCard.innerHTML = `
-//     <h4>${elName.value}</h4>
-//     <p class="relation">${elRelationship.value}</p>
-//     <p id="phone">${elPhone.value}</p>
-//     `;
-//     elCards.appendChild(newCard);
-//     elForm.reset();
-//   } else {
-//     alert("Error");
+  if (elName.value && elRelationship.value && elPhone.value !== "") {
+    const newCard = document.createElement("div");
+    newCard.className = "card";
+    newCard.innerHTML = `
+    <h4>${elName.value}</h4>
+    <p class="relation">${elRelationship.value}</p>
+    <p id="phone">${elPhone.value}</p>
+    `;
+    elCards.appendChild(newCard);
+    elForm.reset();
+  } else {
+    alert("Error");
+  }
+});
+
+// let num = [1, 3, 5, 9];
+
+// function numbersNum(array) {
+//   let sum = 0;
+
+//   for (let i = 0; i < array.length; i++) {
+//     sum += array[i];
 //   }
-// });
-
-let num = [1, 3, 5, 9];
-
-let sum = 0;
-
-for (let i = 0; i < num.length; i++) {
-  sum += num[i];
-}
-console.log(sum);
+//   return sum;
+// }
+// console.log(numbersNum(num));
