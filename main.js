5fcb028ac221b4839711f1e54d14fead67951b90
Buranovv <zahiriddinburanov007@gmail.com>
2023-08-05 21:12:49 +0500

    s

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+const elForm = document.querySelector("#form");
 const elToDo = document.querySelector("#todo");
 const elDate = document.querySelector("#date");
 const elList = document.querySelector(".todoList");
 
-const toDoList = [];
+const toDoList = [1, 2, 3, 4];
 
+function loader(array) {
+  for (let i = 0; i < array.length; i++) {
+    return array[i];
+  }
+}
+console.log(loader(toDoList));
 
+// elForm.addEventListener("submit", function (evt) {
+//   evt.preventDefault();
+
+//   if (elToDo.value && elDate.value !== "") {
+//   }
+// });
