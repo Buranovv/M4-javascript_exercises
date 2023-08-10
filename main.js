d6d74bd00a0b819199f46324737f68e75c3a3f14
Buranovv <zahiriddinburanov007@gmail.com>
2023-08-10 21:53:11 +0500

    fix

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const elCards = document.querySelector(".cards");
+function getElement(element, parent = document) {
+  return parent.querySelector(element);
+}
+const elCards = getElement(".cards");
+const elCard = getElement(".card");
 
-function addData(array) {
+function renderFn(array) {
   for (let i = 0; i < array.length; i++) {
     const element = array[i];
     const newCard = document.createElement("div");
@@ -8,16 +12,18 @@ function addData(array) {
     newCard.style.width = "18rem";
 
     newCard.innerHTML = `
+    <img class="card-img-top" src="${element.image}" alt="Card image cap">
     <div class="card-body">
-          <h5 class="card-title">${element.title}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">${element.price}</h6>
-          <p class="card-text">${element.description}</p>
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
+      <h5 class="card-title">${element.title}</h5>
+      <p class="card-text">${element.price}</p>
+      <p class="card-text">${element.description}</p>
+      <p class="card-text">${element.category}</p>
+      <p class="card-text">${element.rating}</p>
+      <a href="#" class="btn btn-danger">Delete</a>
     </div>
     `;
     elCards.appendChild(newCard);
   }
 }
 
-addData(data);
+renderFn(data);
