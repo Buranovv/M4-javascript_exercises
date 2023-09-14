56eeb3cc4e29951fdff7f13c86a64e63bf4c9e22
Buranovv <zahiriddinburanov007@gmail.com>
2023-09-14 08:21:54 +0500

    done

--- main.js
+++ main.js
@@ -4,32 +4,28 @@ const elPhone = document.querySelector(".phone");
 const elForm = document.querySelector("#form");
 const elCards = document.querySelector(".cards");
 
+class NewCard {
+  addCard(parent, form) {
+    if (elName.value && elRelationship.value && elPhone.value !== "") {
+      const newCard = document.createElement("div");
+      newCard.className = "card";
+      newCard.innerHTML = `
+      <h4>${elName.value}</h4>
+      <p class="relation">${elRelationship.value}</p>
+      <p id="phone">${elPhone.value}</p>
+      `;
+      parent.appendChild(newCard);
+      form.reset();
+    } else {
+      alert("Error");
+    }
+  }
+}
+
+const card = new NewCard();
+
 elForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
 
-  if (elName.value && elRelationship.value && elPhone.value !== "") {
-    const newCard = document.createElement("div");
-    newCard.className = "card";
-    newCard.innerHTML = `
-    <h4>${elName.value}</h4>
-    <p class="relation">${elRelationship.value}</p>
-    <p id="phone">${elPhone.value}</p>
-    `;
-    elCards.appendChild(newCard);
-    elForm.reset();
-  } else {
-    alert("Error");
-  }
+  card.addCard(elCards, elForm);
 });
-
-// let num = [1, 3, 5, 9];
-
-// function numbersNum(array) {
-//   let sum = 0;
-
-//   for (let i = 0; i < array.length; i++) {
-//     sum += array[i];
-//   }
-//   return sum;
-// }
-// console.log(numbersNum(num));
